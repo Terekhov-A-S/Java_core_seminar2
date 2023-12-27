8b3e14094ac2f8dd53192b0aa1875cbc575757c5
KoT <Kotto@student.su>
2023-12-28 01:40:23 +0300

    added folder src

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import tasks.Task1;
+import tasks.Task2;
+import tasks.Task3;
+
+public static void main(String[] args) {
+    System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+    System.out.println("Привет! Сейчас тебе будет предложено ввести несколько значений. Пожалуйста введи их.");
+
+
+
+}
