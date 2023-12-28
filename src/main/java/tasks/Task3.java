4ed51b99195ff6333e5ae773dfb4aeb23a4fcb46
KoT <Kotto@student.su>
2023-12-28 21:43:26 +0300

    edited tasks

--- src/main/java/tasks/Task3.java
+++ src/main/java/tasks/Task3.java
@@ -1,4 +1,26 @@
 package tasks;
 
+// Написать функцию возвращающую истину,
+// если в переданном массиве есть два соседних элемента с нулевым значением.
+
+import java.util.Random;
+
 public class Task3 {
+    public static int[] zeroElement(int[] arrayTwo) {
+        Random random = new Random();
+        int min = 30, max = 0;
+        for (int i = 0; i < arrayTwo.length; i++) {
+            arrayTwo[i] = random.nextInt(30);
+            System.out.print("[" + arrayTwo[i] + "] ");
+            if (arrayTwo[i] > max) {
+                max = arrayTwo[i];
+            } else if (arrayTwo[i] < min) {
+                min = arrayTwo[i];
+            }
+        }
+        int result = max - min;
+        System.out.println("\n" + "Разница между самым большим и самым маленьким элементами второго массива: " + result);
+        return arrayTwo;
+
+    }
 }
